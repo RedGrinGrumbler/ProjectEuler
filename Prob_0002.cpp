e63b3f3e082a9842040ce578d54197039fc50b27
Adam C Smith <36318507+RedGrinGrumbler@users.noreply.github.com>
2019-04-04 00:34:34 -0500

    Added Display Function

--- Prob_0002.cpp
+++ Prob_0002.cpp
@@ -1,46 +1,53 @@
+//RedGrinGrumle
 #include <iostream>
 
 using namespace std;
 
-struct node{
-    int data;
-    struct node *next; // Pointer to next node
-    struct node *prev; // Pointer to previous node
-};
+void display(bool which, int low, int high, int sumTot){
+    //Keeping Code Clean
+    //True: Main Fibonacci Sequence
+    //False: Clearly Show the Additive Values
+    if (which)
+    	printf("\n  Low: %d\tHigh: %d         \tSumTot: %d",
+                low, high, sumTot);
+    else 
+	printf("\t= %d + %d",
+                sumTot, high);
+}
 
 void sumUp(int ceiling, int low, int high, int sumItr = 0, int sumTot = 0){
-    int preSum = sumTot;
-    printf("\n  Low: %d\tHigh: %d         \tSumTot: %d",
-                low, high, sumTot);
+
+    display(true, low, high, sumTot);
+
     sumItr = low + high;
     low = high;
     high = sumItr;
     
     if (low + high < ceiling){
         if (sumItr%2 == 0){
-            printf("\t= %d + %d",
-                sumTot, high);
-            sumTot += sumItr;
-            sumUp(ceiling, low, high, sumItr, sumTot);
-        }else{
-            sumTot = preSum;
-            sumUp(ceiling, low, high, sumItr, sumTot);
-        }
-    }else{
-        if (sumItr%2 == 0){
-            sumTot += sumItr;
-        }
-        printf("\n\n  Low: %d\tHigh: %d \t\tSumTot: %d\n\n",
-                low, high, sumTot);
-        printf("\n\n\tDone");
 
+            display(false, low, high, sumTot);
+
+            sumTot += sumItr;
+            sumUp(ceiling, low, high, sumItr, sumTot);
+
+        }else{
+
+            sumUp(ceiling, low, high, sumItr, sumTot);
+        }
+
+    }else {
+        if (sumItr%2 == 0)
+            sumTot += sumItr;
+        display(true, low, high, sumTot);
+	display(false, low, high, sumTot);
+        printf("\n\n\tDone");
     }
-    return;
+        return; 
 }
 
-int main(){ 
-
+int main()
+{    
     sumUp(4000000, 1, 1);
-
       return 0;
 }
